943332fd80f8a0a43924cbb4fc1a98e6d8776d79
Shruti-1210 <shrutidhamde10@gmail.com>
2023-02-12 19:42:40 +0100

    Arithmetic

---
+++ SeleniumAdvance/src/Arithmetic.java
@@ -0,0 +1,19 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=12,b=2;
+int result=addition(a,b);
+//wSystem.out.println("Result:" + result);
+
+	}
+	
+	public static int addition(int x,int y) {
+		System.out.println(x+y);
+		return x+y;
+	//return x+y;	
+		
+	}
+	
+}
